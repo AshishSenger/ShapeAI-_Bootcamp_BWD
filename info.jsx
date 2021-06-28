449614b3c4cfc6a143914b9646a8caa13297352b
AshishSenger <86344148+AshishSenger@users.noreply.github.com>
2021-06-28 13:20:24 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return(
+  <div className = "note">
+   <h1>
+      Javasript and React.js
+   </h1>
+   <p>
+     Basic Web Development Bootcamp  with React Js 
+   </p>
+   </div>
+  );
+}
+  export default Info;
